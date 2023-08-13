013eadab366711dcddcc6d29fde48daf097eb520
Matheus Nunes <matheusnsilvab@gmail.com>
2023-08-13 16:51:28 -0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+const button = document.getElementById("generate");
+
+button.addEventListener('click', function() {
+  const min = parseInt(document.getElementById("min").value);
+  const max = parseInt(document.getElementById("max").value);
+
+  let result = Math.floor(Math.random() * (max - min + 1)) + min;
+  if(isNaN(result)) {
+    result = "ERROR";
+}
+  document.querySelector("#result > span").textContent = result;
+});
